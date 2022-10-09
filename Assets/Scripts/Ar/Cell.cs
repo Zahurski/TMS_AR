408b9569f3a679286ea9212f50e0266307ccfeec
Zakhar <z.zahurski@gmail.com>
2022-10-10 00:23:44 +0300

    added project

---
+++ Assets/Scripts/Ar/Cell.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ar
+{
+    public enum CellType
+    {
+        None,
+        Cross,
+        Zero
+    }
+
+    public class Cell : MonoBehaviour
+    {
+        [SerializeField] private GameObject cross;
+        [SerializeField] private GameObject zero;
+    
+        public int Index { get; private set; }
+        public bool IsActive { get; set; } = true;
+
+        public CellType Type { get; private set; } = CellType.None;
+
+        public void Initialize(int index)
+        {
+            Index = index;
+        }
+
+        public void SelectCross()
+        {
+            if(!IsActive) return;
+        
+            cross.SetActive(true);
+            zero.SetActive(false);
+            IsActive = false;
+            Type = CellType.Cross;
+        }
+    
+        public void SelectZero()
+        {
+            if(!IsActive) return;
+        
+            zero.SetActive(true);
+            cross.SetActive(false);
+            IsActive = false;
+            Type = CellType.Zero;
+        }
+
+        public void Refresh()
+        {
+            cross.SetActive(false);
+            zero.SetActive(false);
+            IsActive = true;
+            Type = CellType.None;
+        }
+    }
+}
